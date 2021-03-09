3084dda38c542b4716cbd49f4602cec84e694844
bension10 <bension10@gmail.com>
2021-03-10 06:57:13 +0800

    created ScheduleCard component; refactor ScheduleList;

--- client/src/ScheduleList/index.js
+++ client/src/ScheduleList/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
+import ScheduleCard from '../ScheduleCard';
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -9,12 +11,18 @@ const Container = styled.div`
 `;
 
 const ScheduleList = () => {
-  const schedules = useSelector(state => state.schedules);
+  const schedules = useSelector(state => {
+    return state.schedules;
+  });
   console.log('schedules ', schedules);
-
   return (
     <Container>
       <div>Schedule List</div>
+      {
+        schedules.map(schedule => (
+          <ScheduleCard schedule={schedule} key={schedule._id}/>
+        ))
+      }
     </Container>
   );
 };
