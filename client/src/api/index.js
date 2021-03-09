a228387724b11ab76d03819aabe6f4a893d61fe8
bension10 <bension10@gmail.com>
2021-03-10 06:56:22 +0800

    added delete and update on api client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,5 @@ const URL = 'http://localhost:5000/schedules';
 
 export const fetchSchedules = () => axios.get(URL);
 export const createSchedule = schedule => axios.post(URL, schedule);
+export const deleteSchedule = id => axios.delete(`${URL}/${id}`);
+export const updateSchedule = (id, newSchedule) => axios.patch(`${URL}/${id}`, newSchedule);
