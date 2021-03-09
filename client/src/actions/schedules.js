a15f577a875861cefcf8f90c9474715500f58d40
bension10 <bension10@gmail.com>
2021-03-10 06:32:21 +0800

    refactor schedules action

--- client/src/actions/schedules.js
+++ client/src/actions/schedules.js
@@ -1,20 +1,24 @@
 import * as API from '../api';
+import {
+  FETCH_ALL,
+  CREATE_SCHEDULE
+} from '../constants/actionTypes';
 
 export const getSchedules = () => async dispatch => {
   try {
     const { data } = await API.fetchSchedules();
-    dispatch({ type: 'FETCH_ALL', payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
 };
 
 export const createSchedule = schedule => async dispatch => {
-  console.log('schedule ', schedule);
   try {
     const { data } = await API.createSchedule(schedule);
-    dispatch({ type: 'CREATE', payload: data});
-    dispatch(getSchedules);
+
+    dispatch({ type: CREATE_SCHEDULE, payload: data});
+    // dispatch(getSchedules);
   } catch(error) {
     console.log(error.message);
   }
